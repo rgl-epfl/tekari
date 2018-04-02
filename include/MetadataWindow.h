bf43b488a53c4af85c19ce6a81ed6eb86486cd3b
Benoît Ruiz <bruizy@gmail.com>
2018-04-03 01:07:29 +0200

    Don't store metadata in metadataWindow

--- include/MetadataWindow.h
+++ include/MetadataWindow.h
@@ -8,9 +8,8 @@
 class MetadataWindow : public nanogui::Window
 {
 public:
-	MetadataWindow(nanogui::Widget* parent, const DataSample::Metadata& metadata, std::function<void(void)> closeCallback);
+	MetadataWindow(nanogui::Widget* parent, const DataSample::Metadata* metadata, std::function<void(void)> closeCallback);
 
 private:
-	const DataSample::Metadata& m_Metadata;
 	std::function<void(void)> m_CloseCallback;
 };
