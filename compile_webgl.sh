b726a984da188bcd3a47bda33420153e116b7ec4
Wenzel Jakob <wenzel.jakob@epfl.ch>
2018-08-08 18:15:52 +0200

    adjusted compilation script

--- compile_webgl.sh
+++ compile_webgl.sh
@@ -1,2 +1,2 @@
 ninja
-emcc tekari.bc -o tekari.js -s USE_GLFW=3 -s ALLOW_MEMORY_GROWTH=1 -s DISABLE_EXCEPTION_CATCHING=0 --preload-file data_samples/
+emcc tekari.bc -o tekari.html -O3 -s WASM=1 -s USE_GLFW=3 -s ALLOW_MEMORY_GROWTH=1 -s DISABLE_EXCEPTION_CATCHING=0 --preload-file data_samples/
