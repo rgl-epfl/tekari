70a38f964fb15148d1e009340edbd694ea2dffb2
4str0m <bruizy@gmail.com>
2018-08-08 17:01:45 +0200

    Enable optimization when building to webassembly

--- compile_webgl.sh
+++ compile_webgl.sh
@@ -1,2 +1,2 @@
 ninja
-emcc tekari.bc -o tekari.js -s USE_GLFW=3 -s ALLOW_MEMORY_GROWTH=1 -s DISABLE_EXCEPTION_CATCHING=0 --preload-file data_samples/
+emcc -O3 tekari.bc -o tekari.js -s WASM=1 -s USE_GLFW=3 -s ALLOW_MEMORY_GROWTH=1 -s DISABLE_EXCEPTION_CATCHING=0
